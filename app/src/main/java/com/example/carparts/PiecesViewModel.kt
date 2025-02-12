c86dcd659d13428dd9f4f346b76b0e1622243bfd
Theo Foucher <theonicolas.foucher@gmail.com>
2025-02-12 15:20:54 +0100

    Ajout de données en dur dans le viewModel

--- app/src/main/java/com/example/carparts/PiecesViewModel.kt
+++ app/src/main/java/com/example/carparts/PiecesViewModel.kt
@@ -21,8 +21,8 @@ class PiecesViewModel : ViewModel() {
                 "https://fr.wikipedia.org/wiki/Arbre_à_cames",
                 62,
                 424,
-                48.8566,
-                2.3522
+                48.2082,
+                16.3738 // Vienne, Autriche
             ),
             Pieces(
                 "Injecteur",
@@ -31,8 +31,8 @@ class PiecesViewModel : ViewModel() {
                 "https://fr.wikipedia.org/wiki/Injecteur#:~:text=Injecteur%20de%20moteur,-Animation%20d%27un&text=L%27injecteur%20permet%20l%27apport,essence%20qui%20retient%20les%20impuretés.",
                 14,
                 70,
-                51.5074,
-                -0.1278
+                40.7128,
+                -74.0060 // New York, USA
             ),
             Pieces(
                 "Bougie d'allumage",
@@ -41,8 +41,8 @@ class PiecesViewModel : ViewModel() {
                 "https://fr.wikipedia.org/wiki/Bougie_d%27allumage",
                 13,
                 40,
-                40.7128,
-                -74.0060
+                34.0522,
+                -118.2437 // Los Angeles, USA
             ),
             Pieces(
                 "Piston",
@@ -51,8 +51,8 @@ class PiecesViewModel : ViewModel() {
                 "https://fr.wikipedia.org/wiki/Piston_(mécanique)#:~:text=Le%20piston%20est%20l%27élément,un%20mouvement%20rotatif%20du%20vilebrequin.",
                 75,
                 55,
-                42.3601,
-                -83.0841
+                48.8566,
+                2.3522 // Paris, France
             ),
             Pieces(
                 "Vilebrequin",
@@ -61,8 +61,58 @@ class PiecesViewModel : ViewModel() {
                 "https://fr.wikipedia.org/wiki/Vilebrequin_(moteur)#:~:text=Le%20vilebrequin%20est%20un%20dispositif,de%20rotation%20continu%2C%20et%20inversement.",
                 60,
                 500,
-                48.8566,
-                2.3522
+                -33.8688,
+                151.2093 // Sydney, Australie
+            ),
+            Pieces(
+                "Bloc-cylindres",
+                "Le bloc-cylindres est la structure principale du moteur qui abrite les cylindres et d'autres composants essentiels. Il est généralement fabriqué en fonte ou en aluminium pour résister aux hautes températures et pressions générées lors de la combustion.",
+                "bloccylindre",
+                "https://fr.wikipedia.org/wiki/Bloc-cylindres",
+                300,
+                250,
+                55.7558,
+                37.6173 // Moscou, Russie
+            ),
+            Pieces(
+                "Culasse",
+                "La culasse est située au-dessus du bloc-cylindres et renferme les chambres de combustion, les soupapes et, dans certains cas, les arbres à cames. Elle assure l'étanchéité des cylindres et participe au processus de combustion.",
+                "culasse",
+                "https://fr.wikipedia.org/wiki/Culasse",
+                250,
+                300,
+                35.6895,
+                139.6917 // Tokyo, Japon
+            ),
+            Pieces(
+                "Collecteur d'admission",
+                "Le collecteur d'admission distribue le mélange air-carburant aux cylindres du moteur. Il est conçu pour optimiser le flux d'air et améliorer les performances du moteur.",
+                "collecteuradmission",
+                "https://fr.wikipedia.org/wiki/Collecteur_d%27admission",
+                200,
+                600,
+                41.9028,
+                12.4964 // Rome, Italie
+            ),
+            Pieces(
+                "Collecteur d'échappement",
+                "Le collecteur d'échappement recueille les gaz brûlés provenant des cylindres et les dirige vers le système d'échappement. Il est conçu pour résister à des températures élevées et minimiser les pertes de pression.",
+                "collecteurechappement",
+                "https://fr.wikipedia.org/wiki/Collecteur_d%27%C3%A9chappement",
+                200,
+                500,
+                51.5074,
+                -0.1278 // Londres, Royaume-Uni
+            ),
+            Pieces(
+                "Carter d'huile",
+                "Le carter d'huile est un réservoir situé sous le moteur qui contient l'huile de lubrification. Il protège l'huile des contaminants extérieurs et aide à dissiper la chaleur générée par le moteur.",
+                "carterhuile",
+                "https://fr.wikipedia.org/wiki/Carter_d%27huile",
+                300,
+                400,
+                39.9042,
+                116.4074 // Pékin, Chine
             )
         )
         _favorisList.value =
